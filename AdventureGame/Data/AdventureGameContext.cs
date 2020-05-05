655709fbe36340087c3e852ca66d42845069f843
Matthew Justus <matthew.justus@outlook.com>
2020-05-05 09:12:01 -0400

    Database Context

---
+++ AdventureGame/Data/AdventureGameContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AdventureGame.Models;
+
+namespace AdventureGame.Data
+{
+    
+        public class AdventureGameContext : DbContext
+        {
+            public AdventureGameContext(DbContextOptions<AdventureGameContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Item> Item { get; set; }
+            public DbSet<ItemType> ItemType { get; set; }
+        }
+    
+}
---
+++ AdventureGame/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureGame.Models
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public int ItemTypeId { get; }
+    }
+
+    public class ItemType
+    {
+        public int ItemTypeId { get; set; }
+        public string TypeName { get; set; }
+    }
+}
