735273735c31c00625c804e60f744034a514b2dd
Matthew Justus <matthew.justus@outlook.com>
2020-05-05 11:00:17 -0400

    Even more models

---
+++ AdventureGame/Models/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureGame.Models
+{
+    public class Enemy
+    {
+        public int EnemyId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int HP { get; set; }
+        public int Multiplier { get; set; }
+        public int Die { get; set; }
+        public int Modifier { get; set; }
+        public int Exp { get; set; }
+    }
+}
---
+++ AdventureGame/Models/ScreenEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureGame.Models
+{
+    public class ScreenEnemy
+    {
+        public int ScreenEnemyId { get; set; }
+        public int ScreenId { get; set; }
+        public int EnemyId { get; set; }
+        public string Action { get; set; }
+    }
+}
