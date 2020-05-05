f4d752837bd69f4f931e45dc6ee73aa839191506
Matthew Justus <matthew.justus@outlook.com>
2020-05-05 10:53:16 -0400

    Added Models

---
+++ AdventureGame/Models/ScreenItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureGame.Models
+{
+    public class ScreenItem
+    {
+        public int ScreenItemId { get; set; }
+        public int ScreensId { get; set; }
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+        public string TakenDescription { get; set; }
+        public string Action { get; set; }
+
+    }
+}
---
+++ AdventureGame/Models/Screens.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureGame.Models
+{
+    public class Screens
+    {
+        public int ScreensId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
